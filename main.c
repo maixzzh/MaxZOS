7dddb55beaf3f5d2fcb00a5a214bd6eb219e4721
maixzzh <maixuanzhang7@gmail.com>
2026-08-22 08:56:57 +0800

    更新：在内核加载完成时会丢掉显存

--- main.c
+++ main.c
@@ -5,24 +5,30 @@
 
 #define VGA_MEMORY 0xB8000
 #define VGA_ATTR   0x0F
-#define COLS       80      // 每行字符数
-#define ROWS       25      // 总行数
+#define COLS       80
+#define ROWS       25
+#define SCR_SIZE   (COLS * ROWS) 
 
 void kmain(unsigned long magic, unsigned long addr) {
     volatile unsigned short* vga = (volatile unsigned short*)VGA_MEMORY;
+        for (int i = 0; i < SCR_SIZE; i++) {
+        vga[i] = (VGA_ATTR << 8) | ' ';
+    }
     const char* message = "Hello, World!\nThis is zmx's OS.";
     unsigned int i = 0;
-    unsigned int pos = 0; 
+    unsigned int pos = 0;
 
     while (message[i] != '\0') {
         if (message[i] == '\n') {
             pos = ((pos / COLS) + 1) * COLS;
         } else {
             vga[pos] = (VGA_ATTR << 8) | message[i];
-            pos++; 
+            pos++;
         }
         i++;
     }
+
+    // 内核完成，进入死循环
     while (1) {
         asm volatile ("hlt");
     }
